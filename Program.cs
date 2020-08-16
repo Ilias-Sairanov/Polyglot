8fe0df3d63f233a62192516a4484e82c30a45ebe
Ilias Sairanov <ilyas.sayranov@inbox.ru>
2020-08-16 22:32:19 +0700

    add objects

--- Program.cs
+++ Program.cs
@@ -1,16 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Polyglot
 {
+    class Task
+    {
+
+    }
+
+    class TaskBuilder
+    {
+
+    }
+
+    class GameHost
+    {
+        private List<string[]> verbs;
+
+        public GameHost()
+        {
+            verbs = new List<string[]>();
+            var file = File.ReadAllText("Verbs.txt").Replace("\r", "");
+            var splitVerbs = file.Split('\n');
+            foreach (var verb in splitVerbs)
+            {
+                var splittedByFormsVerb = verb.Split('#');
+                verbs.Add(splittedByFormsVerb);
+            }
+        }
+
+        public string[] GetVerb()
+        {
+            var rnd = new Random();
+            string[] verb = verbs[rnd.Next(verbs.Count)];
+            return verb;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testing branches");
+            var host = new GameHost();
         }
+    }
+
+    enum SentenceTime
+    {
+        Present,
+        Past,
+        Future
+    }
+
+    enum SentenceType
+    {
+        Affirmative,
+        Negative,
+        Question
     }
 }
