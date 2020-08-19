5d58dfe97a00de9dc545261ce2a95d9f3026dedc
Ilias Sairanov <ilyas.sayranov@inbox.ru>
2020-08-19 21:31:33 +0700

    add taskbuilder

--- Program.cs
+++ Program.cs
@@ -1,26 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Polyglot
 {
     class Task
     {
+        public SentenceType SentenceType { get; }
+        public SentenceTime SentenceTime { get; }
+        public string[] Pronoun { get; }
+        public string[] Verb { get; }
 
+        public Task(string[] verb, string[] pronoun)
+        {
+            var rnd = new Random();
+            SentenceTime = (SentenceTime)rnd.Next(3);
+            SentenceType = (SentenceType)rnd.Next(3);
+            Pronoun = pronoun;
+            Verb = verb;
+            for (int i = 0; i < Verb.Length; i++)
+            {
+                Verb[i] = Verb[i].Substring(0, 1).ToUpper() + Verb[i].Substring(1);
+            }
+        }
     }
 
     class TaskBuilder
     {
-
+        public string GetTask(Task task, TaskType taskType)
+        {
+            string str = string.Format($"Время: {task.SentenceTime}\n" +
+                $"Тип: {task.SentenceType}\n" +
+                $"Местоимение: {task.Pronoun[0]}\n" +
+                $"Глагол: {task.Verb[0]}");
+            return str;
+        }
     }
 
     class GameHost
     {
         private List<string[]> verbs;
+        private string[] pronounsEng = { "I", "You", "We", "They", "He", "She", "It" };
+        private string[] pronounsRus = { "Я", "Вы", "Мы", "Они", "Он", "Она", "Оно" };
 
         public GameHost()
         {
@@ -40,15 +61,38 @@ namespace Polyglot
             string[] verb = verbs[rnd.Next(verbs.Count)];
             return verb;
         }
+
+        public string[] GetPronoun()
+        {
+            var rnd = new Random();
+            int i = rnd.Next(pronounsEng.Length);
+            string[] pronoun = { pronounsRus[i], pronounsEng[i] };
+            return pronoun;
+        }
     }
-
-
 
     class Program
     {
         static void Main(string[] args)
         {
             var host = new GameHost();
+            var task = new Task(host.GetVerb(), host.GetPronoun());
+            var bilder = new TaskBuilder();
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                Console.WriteLine(bilder.GetTask(task, TaskType.First));
+                var typedKey = Console.ReadKey();
+                switch (typedKey.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("check");
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine();
+            }
         }
     }
 
@@ -64,5 +108,11 @@ namespace Polyglot
         Affirmative,
         Negative,
         Question
+    }
+
+    enum TaskType
+    {
+        First,
+        Second
     }
 }
