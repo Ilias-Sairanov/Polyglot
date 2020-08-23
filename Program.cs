db8eb503ca13e8462cf10d744aeb299b014bb420
Ilias Sairanov <ilyas.sayranov@inbox.ru>
2020-08-23 18:34:58 +0700

    delete host, add randomizer

--- Program.cs
+++ Program.cs
@@ -6,44 +6,16 @@ namespace Polyglot
 {
     class Task
     {
-        public SentenceType SentenceType { get; }
-        public SentenceTime SentenceTime { get; }
-        public string[] Pronoun { get; }
-        public string[] Verb { get; }
+        public SentenceType SentenceType { get; private set; }
+        public SentenceTime SentenceTime { get; private set; }
+        public string[] Pronoun { get; private set; }
+        public string[] Verb { get; private set; }
 
-        public Task(string[] verb, string[] pronoun)
-        {
-            var rnd = new Random();
-            SentenceTime = (SentenceTime)rnd.Next(3);
-            SentenceType = (SentenceType)rnd.Next(3);
-            Pronoun = pronoun;
-            Verb = verb;
-            for (int i = 0; i < Verb.Length; i++)
-            {
-                Verb[i] = Verb[i].Substring(0, 1).ToUpper() + Verb[i].Substring(1);
-            }
-        }
-    }
-
-    class TaskBuilder
-    {
-        public string GetTask(Task task, TaskType taskType)
-        {
-            string str = string.Format($"Время: {task.SentenceTime}\n" +
-                $"Тип: {task.SentenceType}\n" +
-                $"Местоимение: {task.Pronoun[0]}\n" +
-                $"Глагол: {task.Verb[0]}");
-            return str;
-        }
-    }
-
-    class GameHost
-    {
         private List<string[]> verbs;
         private string[] pronounsEng = { "I", "You", "We", "They", "He", "She", "It" };
         private string[] pronounsRus = { "Я", "Вы", "Мы", "Они", "Он", "Она", "Оно" };
 
-        public GameHost()
+        public Task()
         {
             verbs = new List<string[]>();
             var file = File.ReadAllText("Verbs.txt").Replace("\r", "");
@@ -53,21 +25,49 @@ namespace Polyglot
                 var splittedByFormsVerb = verb.Split('#');
                 verbs.Add(splittedByFormsVerb);
             }
+
+            var rnd = new Random();
+            SentenceTime = (SentenceTime)rnd.Next(3);
+            SentenceType = (SentenceType)rnd.Next(3);
+            int random = rnd.Next(pronounsEng.Length);
+            Pronoun = new string[] { pronounsRus[random], pronounsEng[random] };
+            Verb = verbs[rnd.Next(verbs.Count)];
+            for (int i = 0; i < Verb.Length; i++)
+            {
+                Verb[i] = Verb[i].Substring(0, 1).ToUpper() + Verb[i].Substring(1);
+            }
         }
 
-        public string[] GetVerb()
+        public void NextTask()
         {
             var rnd = new Random();
-            string[] verb = verbs[rnd.Next(verbs.Count)];
-            return verb;
+            SentenceTime = (SentenceTime)rnd.Next(3);
+            SentenceType = (SentenceType)rnd.Next(3);
+            int random = rnd.Next(pronounsEng.Length);
+            Pronoun = new string[] { pronounsRus[random], pronounsEng[random] };
         }
 
-        public string[] GetPronoun()
+        public void NewVerb()
         {
+            NextTask();
             var rnd = new Random();
-            int i = rnd.Next(pronounsEng.Length);
-            string[] pronoun = { pronounsRus[i], pronounsEng[i] };
-            return pronoun;
+            Verb = verbs[rnd.Next(verbs.Count)];
+            for (int i = 0; i < Verb.Length; i++)
+            {
+                Verb[i] = Verb[i].Substring(0, 1).ToUpper() + Verb[i].Substring(1);
+            }
+        }
+    }
+
+    class TaskBuilder
+    {
+        public string GetTask(Task task)
+        {
+            string str = string.Format($"Время: {task.SentenceTime}\n" +
+                $"Тип: {task.SentenceType}\n" +
+                $"Местоимение: {task.Pronoun[0]}\n" +
+                $"Глагол: {task.Verb[0]}");
+            return str;
         }
     }
 
@@ -75,19 +75,22 @@ namespace Polyglot
     {
         static void Main(string[] args)
         {
-            var host = new GameHost();
-            var task = new Task(host.GetVerb(), host.GetPronoun());
+            var task = new Task();
             var bilder = new TaskBuilder();
             bool gameOver = false;
             while (!gameOver)
             {
-                Console.WriteLine(bilder.GetTask(task, TaskType.First));
+                Console.WriteLine(bilder.GetTask(task));
+                Console.WriteLine("1: новое задание\t2:заменить глагол");
                 var typedKey = Console.ReadKey();
                 Console.WriteLine();
                 switch (typedKey.Key)
                 {
                     case ConsoleKey.D1:
-                        Console.WriteLine("check");
+                        task.NextTask();
+                        break;
+                    case ConsoleKey.D2:
+                        task.NewVerb();
                         break;
                     default:
                         break;
@@ -109,11 +112,5 @@ namespace Polyglot
         Affirmative,
         Negative,
         Question
-    }
-
-    enum TaskType
-    {
-        First,
-        Second
     }
 }
